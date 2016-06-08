5fe743dcdd6235b24767e7ac9e7ddffecbb8ce69
annefried <annemarie.friedrich@gmail.com>
2016-06-08 16:27:20 +0200

    initial import

---
+++ de.uni-saarland.coli.sitent/src/main/java/de/uniSaarland/coli/de/sitent/App.java
@@ -0,0 +1,13 @@
+package de.uniSaarland.coli.de.sitent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
