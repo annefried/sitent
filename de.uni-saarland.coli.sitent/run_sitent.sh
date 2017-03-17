b55bdf121cc9470613df9b8f8417e959c406f284
Annemarie Friedrich <friedricha@cip.ifi.lmu.de>
2017-03-17 14:41:19 +0100

    changed command line parameters for experimenter

--- de.uni-saarland.coli.sitent/run_sitent.sh
+++ de.uni-saarland.coli.sitent/run_sitent.sh
@@ -52,6 +52,7 @@ COUNTABILITY_PATH=resources/countability/webcelex_countabilityNouns.txt
 # Here, you need to put the directory where you installed CRF++ on your 
 # system, see above.
 CRFPP_INSTALL_DIR=~/software/CRF++-0.58-fixed
+LIBLINEAR_INSTALL_DIR=~/software/liblinear
 CRFSUITE_INSTALL_DIR=~/software/crfsuite-0.12/bin
 
 # --------------- #
@@ -76,6 +77,6 @@ python3 python-scripts/split.py $ARFF-compatible $TRAIN_TEST_SPLIT $ARFF"_split"
 
 # Step 4: run experiment: filter data according to configured features, train classifier, develop using 10-fold-CV, or apply on test set.
 
-java -jar target/de.uni-saarland.coli.sitent-0.0.1-SNAPSHOT-experimenter.jar $EXPERIMENT_CONFIG $CRFPP_INSTALL_DIR $CRFSUITE_INSTALL_DIR
+java -jar target/de.uni-saarland.coli.sitent-0.0.1-SNAPSHOT-experimenter.jar $EXPERIMENT_CONFIG $CRFPP_INSTALL_DIR $LIBLINEAR_INSTALL_DIR $CRFSUITE_INSTALL_DIR
 
 # Results will be in the $EXPERIMENT_CONFIG folder: look in the folders created for each experiment there!
